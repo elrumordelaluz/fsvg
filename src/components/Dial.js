658600ca8d572bb6339d4c18a1b221e97e3a2baf
Lionel T <elrumordelaluz@hotmail.com>
2016-04-08 23:32:54 +0200

    Add Dial-knob

--- src/components/Dial.js
+++ src/components/Dial.js
@@ -3,11 +3,23 @@ import React, { Component } from 'react';
 class Dial extends Component {
   constructor (props) {
     super(props);
+
+    this.state = {
+      inUse: false
+    }
+  }
+
+  componentDidMount () {
+    this.x = 0;
+    this.y = 0;
   }
 
   render () {
     return (
-      <div>Hello!</div>
+      <svg className="Dial"ref={ (knob) => this.knob = knob } >
+        <circle className="Dial-total" />
+        <circle className="Dial-quantity" />
+      </svg>
     );
   }
 }
